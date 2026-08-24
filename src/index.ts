752a80e9d9ee6d0eaa7cf566ceaa01af18a0caef
Abhishek_Timsina <timsinaabhishek1@gmail.com>
2026-08-24 13:33:18 +0545

    seprate the db logic and main file

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,14 @@ import express from "express"
 import { Express } from "express"
 import {getEnvProperty} from "./utils/get.env.utils.js"
 import { connectToDatabase } from "./database/connect.js"
-import { DatabaseException } from "./exception/exception.js"
-import { HTTP_STATUS } from "./constants/http-status.constants.js"
+
 import { serverMiddleware } from "./middleware/server.middleware.js"
 import { serverRoute } from "./routes/server.routes.js"
 
 export const appConfiguration = async() => {
    try{
     const app : Express = express()
-    const db_status = await connectToDatabase()
-    if(!db_status){
-        throw new DatabaseException("Database couldnot be connected❌.", HTTP_STATUS.SERVER_ERROR.INTERNAL_SERVER_ERROR.CODE)
-    }
+    await connectToDatabase()
     serverMiddleware(app)
     serverRoute(app)
     app.listen(getEnvProperty("port"), (err) => {
