ad58ceb61916b8fafd10c8a788f546bf9a4bc3de
Abhishek_Timsina <timsinaabhishek1@gmail.com>
2026-08-25 14:25:47 +0545

    verify token

--- src/utils/jwt.utils.ts
+++ src/utils/jwt.utils.ts
@@ -1,7 +1,8 @@
-import { IjwtData } from "../interface/interfaces";
+import { IjwtData, IPayload } from "../interface/interfaces";
 import jwt from "jsonwebtoken"
 import { roleType } from "../types/types";
 import { getEnvProperty } from "./get.env.utils";
+import { AuthenticationError } from "../exception/exception";
 
 export const generateToken = (userInfo : IjwtData, role : roleType) => {
     const access_token = jwt.sign({...userInfo, role}, getEnvProperty("access_token_secret_key"),{
@@ -11,4 +12,23 @@ export const generateToken = (userInfo : IjwtData, role : roleType) => {
         expiresIn : "30d"
     })
     return {access_token, refresh_token}
+}
+
+export const verifyToken = (access_token : string) => {
+    try{
+        const payload = jwt.verify(access_token, getEnvProperty("access_token_secret_key"))
+        console.log(payload)
+        return payload as IPayload
+    }
+    catch(err){
+        if(err instanceof jwt.TokenExpiredError){
+            throw new AuthenticationError("TOKEN_EXPIRED", "Token has expired, please refresh")
+        }
+        else if(err instanceof jwt.NotBeforeError){
+            throw new AuthenticationError("TOKEN_NOT_READY", "Token has expired")
+        }
+        else{
+            throw new AuthenticationError("TOKEN_INVALID", "Token has been tampered")
+        }
+    }
 }
