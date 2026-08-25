141adf8abf0ef200f0dff456ec70eff11732fa89
Abhishek_Timsina <timsinaabhishek1@gmail.com>
2026-08-25 14:29:10 +0545

    exception

--- src/exception/exception.ts
+++ src/exception/exception.ts
@@ -16,10 +16,10 @@ export class DatabaseException<T> extends Error {
     }
 }
 
-export class APIError<T> extends Error {
+export class APIError extends Error {
     statusCode : number
-    details : genericAndNull<T>
-    constructor(message : string, statusCode : number, details : genericAndNull<T> = null){
+    details : any
+    constructor(message : string, statusCode : number, details : any = null){
         super(message)
         this.name = "APIError"
         this.statusCode = statusCode
@@ -27,21 +27,19 @@ export class APIError<T> extends Error {
     }
 }
 
-// export class ValidationError<T> extends Error{
-//     name : string
-//     message : string
-//     statusCode : number
-//     details : genericAndNull<T>
+export class ValidationError extends APIError{
+    constructor(error : Joi.ValidationError){
+        super(error.message, HTTP_STATUS.CLIENT_ERROR.BAD_REQUEST.CODE, { 
+            type : error.details[0].type,
+            context : error.details[0].context
+        })
+        this.name = "VALIDATION_ERROR"
+    }
+}
 
-//     constructor(error : Joi.ValidationError){
-//         super(error.message)
-//         this.name = "ValidationError"
-//         this.message = error.message
-//         this.statusCode = HTTP_STATUS.CLIENT_ERROR.BAD_REQUEST.CODE
-//         this.details = { 
-//             type : error.details[0].type,
-//             context : error.details[0].context
-//         }
-
-//     }
-// }
+export class AuthenticationError extends APIError{
+    constructor(name : string, message : string){
+        super(message, HTTP_STATUS.CLIENT_ERROR.UNAUTHORIZED.CODE)
+        this.name = name
+    }
+}
