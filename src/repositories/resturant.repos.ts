514f4c168e79552b2a22e9424e2b9d9cb4d45f36
Abhishek_Timsina <timsinaabhishek1@gmail.com>
2026-08-25 23:13:02 +0545

    get repos with filter

--- src/repositories/resturant.repos.ts
+++ src/repositories/resturant.repos.ts
@@ -2,6 +2,9 @@ import { Repository } from "typeorm";
 import { Resturant } from "../database/Entity/resturant.entity";
 import { AppDataSource } from "../database/connect";
 import { IResturantSignUp } from "../interface/interfaces";
+import { Role } from "../enums/enums";
+import { allResturantsSelect, resturantSelect } from "../constants/project.constants";
+import { IResturantFilterType } from "../types/types";
 
 
 export class ResturantRepository {
@@ -10,32 +13,60 @@ export class ResturantRepository {
         this.resturantRepo = AppDataSource.getRepository(Resturant)
     }
 
-    public findResturant = async (owner_name : string, resturant_name: string, IsPassword : boolean = false) : Promise<Resturant> => {
+    public findResturantDetailed = async <T>(key : string, value : T, role : Role ) => {
         const resturant = await this.resturantRepo.findOne({
             where : {
-                owner_name : owner_name,
-                resturant_name : resturant_name
+                [`${key}`] : value
+            },
+            select : {
+                ...resturantSelect[role]
+            },
+            relations : {
+                address : true,
+                items : true,
+                orders : true
+            }
+        })
+        return resturant
+    }
+
+    // signUp garda kheri
+    public findResturant =  async <T>(key : string, value : T, IsPassword : boolean = false) : Promise<Resturant> => {
+        const resturant = await this.resturantRepo.findOne({
+            where : {
+                [`${key}`] : value
             },
             select : {
                 resturant_id : true,
                 resturant_name : true,
                 status : true,
-                password : IsPassword ? true : false,
-                address : {
-                    city : true,
-                    ward_num : true,
-                    phone_number1 : true,
-                    street_name : true
-                },
+                password : (IsPassword) ? true : false,
                 approval_status : true,
                 approved_by : true,
-                created_at : true
             },
-            relations : {
-                address : true
-            }
         })
         return resturant
+    }
+
+    public findResturants = async (role : Exclude<Role, Role.RESTURANT>, filter : IResturantFilterType & { approved_status : boolean}) => {
+        const resturants = await this.resturantRepo.find({
+            where : {
+                ...filter
+            },
+            select : {
+                ...allResturantsSelect[role]
+            }
+        })
+        return resturants
+    }
+
+    public checkResturant = async (id : number) => {
+        const IsResturant = await this.resturantRepo.exists({
+            where : {
+                resturant_id : id as number
+            }
+        })
+        return IsResturant 
     }
 
     public createResturant = async (restrodData : Omit<IResturantSignUp, "address">) => {
