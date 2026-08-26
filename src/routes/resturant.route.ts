dc31106d7436e84bd8432e6911661893b8d78836
Abhishek_Timsina <timsinaabhishek1@gmail.com>
2026-08-26 19:43:05 +0545

    resturant route

--- src/routes/resturant.route.ts
+++ src/routes/resturant.route.ts
@@ -1,3 +1,11 @@
 import { Router } from "express";
+import { deleteResturant, getResturant, getResturants, updateResturant, updateResturantAdmin } from "../controller/resturants.controller";
+import { authenticationMiddleware } from "../middleware/auth.middleware";
 
 export const resturantRouter = Router()
+
+resturantRouter.get("/resturants",authenticationMiddleware, getResturants)
+resturantRouter.get("/resturant/:id", authenticationMiddleware as any, getResturant)
+resturantRouter.patch("/resturant", authenticationMiddleware as any, updateResturant)
+resturantRouter.patch("/resturant/:id", authenticationMiddleware as any, updateResturantAdmin)
+resturantRouter.delete("/resturant/:id", authenticationMiddleware as any, deleteResturant)
