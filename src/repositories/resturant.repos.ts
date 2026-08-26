f8845fbaf92ffcc63c7ad72ceae1de814e3ad18d
Abhishek_Timsina <timsinaabhishek1@gmail.com>
2026-08-26 19:43:57 +0545

    resturant repos

--- src/repositories/resturant.repos.ts
+++ src/repositories/resturant.repos.ts
@@ -1,10 +1,10 @@
 import { Repository } from "typeorm";
 import { Resturant } from "../database/Entity/resturant.entity";
 import { AppDataSource } from "../database/connect";
-import { IResturantSignUp } from "../interface/interfaces";
-import { Role } from "../enums/enums";
+import { IResturantSignUp, IResturantUpdate } from "../interface/interfaces";
+import { RESTURANT_IS_APPROVED, Role } from "../enums/enums";
 import { allResturantsSelect, resturantSelect } from "../constants/project.constants";
-import { IResturantFilterType } from "../types/types";
+import { IResturantFilterType, IResturantUpdateType } from "../types/types";
 
 
 export class ResturantRepository {
@@ -43,18 +43,22 @@ export class ResturantRepository {
                 password : (IsPassword) ? true : false,
                 approval_status : true,
                 approved_by : true,
+                created_at : true
             },
         })
         return resturant
     }
 
-    public findResturants = async (role : Exclude<Role, Role.RESTURANT>, filter : IResturantFilterType & { approved_status : boolean}) => {
+    public findResturants = async (role : Exclude<Role, Role.RESTURANT>, filter : IResturantFilterType & { approval_status : RESTURANT_IS_APPROVED.RESTURANT_APPROVED}) => {
         const resturants = await this.resturantRepo.find({
             where : {
                 ...filter
             },
             select : {
                 ...allResturantsSelect[role]
+            },
+            relations : {
+                address : true
             }
         })
         return resturants
@@ -78,5 +82,22 @@ export class ResturantRepository {
         const resturants = await this.resturantRepo.find()
         await this.resturantRepo.remove(resturants)
         return resturants
+    }
+
+    public updateResturant = async (restroData : IResturantUpdateType, resturant_id : number) => {
+        await this.resturantRepo.update({
+            resturant_id : resturant_id
+        },
+        {
+            ...restroData
+        })
+        const resturant = await this.findResturant("resturant_id", resturant_id)
+        return resturant
+    }
+
+    public deleteResturant = async (resturant_id : number) => {
+        const resturant = await this.findResturant("resturant_id", resturant_id)
+        await this.resturantRepo.remove(resturant)
+        return resturant
     }
 }
