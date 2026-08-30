2bae995584a7a35961116e480b74e4671bf50f5a
Abhishek_Timsina <timsinaabhishek1@gmail.com>
2026-08-30 21:38:33 +0545

    webhook route

---
+++ src/routes/webhook.routes.ts
@@ -0,0 +1,19 @@
+import { Request, Response, Router } from "express";
+import express from "express"
+import { payment } from "../controller/webhooks.controller";
+
+
+export const webhookRouter = Router()
+
+webhookRouter.post("/payment", express.raw({type : "application/json"}), payment)
+webhookRouter.get("/success", (req : Request, res : Response) => {
+    res.json({
+        "message" : "payment done"
+    })
+})
+
+webhookRouter.get("/cancel", (req : Request, res : Response) => {
+    res.json({
+        "error" : "payment canceled"
+    })
+})
