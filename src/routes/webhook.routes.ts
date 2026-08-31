823e84d25a34a5e4ca5de6e043cb8ba12bd8e3ff
Abhishek_Timsina <timsinaabhishek1@gmail.com>
2026-08-31 22:59:08 +0545

    fixing the parse for stripe

--- src/routes/webhook.routes.ts
+++ src/routes/webhook.routes.ts
@@ -1,18 +1,21 @@
-import { Request, Response, Router } from "express";
+import { NextFunction, Request, Response, Router } from "express";
 import express from "express"
 import { payment } from "../controller/webhooks.controller";
 
 
 export const webhookRouter = Router()
 
-webhookRouter.post("/payment", express.raw({type : "application/json"}), payment)
-webhookRouter.get("/success", (req : Request, res : Response) => {
+webhookRouter.post("/webhook/payment", express.raw({type : "application/json"}),(req : Request, res : Response, next : NextFunction) => {
+    console.log("hook ma event aayo hai")
+    next()
+}, payment)
+webhookRouter.get("/webhook/success", (req : Request, res : Response) => {
     res.json({
         "message" : "payment done"
     })
 })
 
-webhookRouter.get("/cancel", (req : Request, res : Response) => {
+webhookRouter.get("/webhook/cancel", (req : Request, res : Response) => {
     res.json({
         "error" : "payment canceled"
     })
