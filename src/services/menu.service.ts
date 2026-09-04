928608c18ae8df8ff2122f677f82ce64848b8993
Abhishek_Timsina <timsinaabhishek1@gmail.com>
2026-09-04 23:11:27 +0545

    redis implementation ez

--- src/services/menu.service.ts
+++ src/services/menu.service.ts
@@ -3,6 +3,8 @@ import { MenuRepository } from "../repositories/menu.repos";
 import { IuserData } from "../types/types";
 import { IMenuData } from "../interface/interfaces";
 import { APIError } from "../exception/exception";
+import { redisClient } from "../config/redis.config";
+import { json } from "node:stream/consumers";
 
 export class MenuService {
     menuRepo : MenuRepository
@@ -13,11 +15,23 @@ export class MenuService {
     public getItems = async (menuData : IuserData) => {
         const {id} = menuData
         console.log(id)
+
+        // use the redis to check hit or miss
+        const cachedResturatn = await redisClient.get("menu")
+        if(cachedResturatn){
+            return JSON.parse(cachedResturatn) 
+        }
+        console.log("menu repo")
         const data  = await this.menuRepo.getMenu(id)
         if(!data){
             throw new APIError("menu not found", 404)
         }
         const {items} = data
+        // miss so for the next time we set the data into the cache
+        redisClient.set("menu", JSON.stringify(items))
+
+        // delete whenever we wanna
+        // redisClient.del("menu")
         return items
     }
 
@@ -30,6 +44,7 @@ export class MenuService {
     public createItem = async (userData : IuserData, menuData : IMenuData) => {
         const {id} = userData
         const menu = await this.menuRepo.createMenu(menuData, +id)
+        await redisClient.del("menu")
         return {
             userData,
             menuData
